5e0b6431e4d791d80ae15cc0f888d6247dd4dfa9
YunWZ <verdgun@gmail.com>
2020-02-24 22:42:55 +0800

    增加优化版快速排序

---
+++ src/com/github/verdgun/sort/QuickXSort.java
@@ -0,0 +1,75 @@
+package com.github.verdgun.sort;
+
+/**
+ * 快速排序(优化版)
+ */
+public class QuickXSort implements ISort {
+    @Override
+    public Comparable[] sort(Comparable[] unsorted) {
+        return sort(unsorted, 0, unsorted.length - 1);
+    }
+
+    private Comparable[] sort(Comparable[] unsorted, int lo, int hi) {
+        if (lo >= hi) {
+            return unsorted;
+        }
+
+        //优化2.3.3.1如果数组长度过小,则直接使用插入排序
+        if ((hi - lo + 1) < INSERTION_SORT_CUTOFF) {
+            return (new InsertSort()).sort(unsorted, lo, hi);
+        }
+
+        int j = partition(unsorted, lo, hi);
+        sort(unsorted, lo, j - 1);
+        sort(unsorted, j + 1, hi);
+
+        return unsorted;
+    }
+
+    private int partition(Comparable[] unsorted, int lo, int hi) {
+        //优化: 取中位数,并将该值作为切分元素
+        int m = median3(unsorted, lo, hi);
+        exch(unsorted, lo, m);
+
+
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = unsorted[lo];
+        //找到第一个大于等于v的元素
+        while (less(unsorted[++i], v)) {
+            //如果最后一个元素依然小于v,说明unsorted[(lo+1)...hi]都小于v,交换unsorted[lo]和unsorted[hi]之后,直接返回切分后的元素下标hi
+            if (i == hi) {
+                exch(unsorted, i, lo);
+                return i;
+            }
+        }
+
+        ////找到第一个小于等于v的元素
+        while (less(v, unsorted[--j])) {
+            //如果unsorted[lo + 1]依然大于v(unsorted[lo])则,说明unsorted[(lo+1)...hi]都大于v,直接返回切分后的元素下标lo
+            if (j == lo + 1) {
+                return lo;
+            }
+        }
+
+        //如果i>=j,说明元素切分结束
+        while (i < j) {
+            //执行交换后,unsorted[i] <= v <= unsorted[j],且此时i < j
+            exch(unsorted, i, j);
+            //因此,此内循环不会发生数组下标越界(因为必然存在unsorted[j] >= v,从而使得循环终结)
+            while (less(unsorted[++i], v)) ;
+            while (less(v, unsorted[--j])) ;
+        }
+
+        exch(unsorted, j, lo);
+        return j;
+    }
+
+    private int median3(Comparable[] unsorted, int lo, int hi) {
+        int m = lo + (hi - lo + 1) / 2;
+
+        int min = less(unsorted[lo], unsorted[m]) ? lo : m;
+        min = less(min, unsorted[hi]) ? min : hi;
+        return min;
+    }
+}
