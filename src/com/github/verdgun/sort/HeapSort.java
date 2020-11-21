64f99afc2960fdc0221fd7521772f5733edceb6d
YunWZ <verdgun@gmail.com>
2020-11-21 17:09:12 +0800

    堆排序增加算法注释

--- src/com/github/verdgun/sort/HeapSort.java
+++ src/com/github/verdgun/sort/HeapSort.java
@@ -1,34 +1,28 @@
 package com.github.verdgun.sort;
 
+/**
+ * 堆排序
+ */
 public class HeapSort<T extends Comparable<T>> implements ISort<T> {
     @Override
     public T[] sort(T[] unsorted) {
         int length = unsorted.length;
-        //堆初始化(大根堆)
+        //堆初始化(大根堆),ceil(length / 2)为最后一个非叶子节点
         for (int i = length / 2; i > 0; i--) {
             sink(unsorted, i, length);
         }
 
-        for (int i = length; i > 1; ) {
-            exchange(unsorted, 1, i--);
-            sink(unsorted, 1, i);
+        //经过上一个for循环排序，根节点为最大元素
+        //将根元素和最后的一个元素交换位置，并将其从堆中移除，再重新排序为大根堆
+        for (int index = length; index > 1; --index) {
+            exchange(unsorted, 0, index - 1);
+            sink(unsorted, 1, index - 1);
         }
         return unsorted;
     }
 
-    public boolean less(T[] unsorted, int i, int j) {
-        return less(unsorted[i - 1], unsorted[j - 1]);
-    }
-
-    @Override
-    public void exchange(T[] a, int i, int j) {
-        T temp = a[i - 1];
-        a[i - 1] = a[j - 1];
-        a[j - 1] = temp;
-    }
-
     /**
-     * 按小根堆排序
+     * 将当前节点下沉到具体位置
      *
      * @param unsorted 待排序数组
      * @param i        当前节点
@@ -36,11 +30,16 @@ public class HeapSort<T extends Comparable<T>> implements ISort<T> {
      */
     private void sink(T[] unsorted, int i, int end) {
         while (2 * i <= end) {
+            //2i为做当前节点的左子节点
             int child = 2 * i;
-            //取出
-            if (child < end && less(unsorted, child, child + 1)) child++;
-            if (!less(unsorted, i, child)) break;
-            exchange(unsorted, i, child);
+            //取出  (child < end)--防止超出节点范围，左子节点在数组中的位置为child-1，右子节点在数组中的位置为child+1-1=child
+            //如果左子节点小于右子节点，则另child指向右子节点
+            if (child < end && lessThen(unsorted[child - 1], unsorted[child])) child++;
+
+
+            //如果当前节点大于最大的子节点，则结束排序,否则交换元素exchange(unsorted, i, child)，并递归下去
+            if (!lessThen(unsorted[i - 1], unsorted[child - 1])) break;
+            exchange(unsorted, i - 1, child - 1);
             i = child;
         }
     }
