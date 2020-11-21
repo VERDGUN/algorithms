30e6ab8f22f79631416f283a646e1ccaf9927fe3
YunWZ <verdgun@gmail.com>
2020-11-21 19:05:33 +0800

    快速排序代码优化

--- src/com/github/verdgun/sort/Quick3WaySort.java
+++ src/com/github/verdgun/sort/Quick3WaySort.java
@@ -9,29 +9,31 @@ public class Quick3WaySort<T extends Comparable<T>> implements ISort<T> {
 
     private T[] sort(T[] unsorted, int lo, int hi) {
         if (lo >= hi) return unsorted;
-        T v = unsorted[lo];
-        int lt = lo, gt = hi, i = lo + 1;
-        while (i <= gt) {
-            int compareTo = unsorted[i].compareTo(v);
-            // 如果小于v,则将元素unsorted[i]和数组左侧的unsorted[lt]交换
-            // 因为大于v的元素会移到右侧子数组unsorted[gt...hi]中,因此unsorted[i]和unsorted[lt]交换后依然能保证unsorted[lo,lt]小于等于v
-            // 所以将lt,i指向下一个元素即可,不需要再次比较unsorted[i]和v的关系
+        T current = unsorted[lo];
+        int lessIndex = lo;
+        int greateIndex = hi;
+        int index = lo + 1;
+        while (index <= greateIndex) {
+            int compareTo = unsorted[index].compareTo(current);
+            // 如果小于current,则将元素unsorted[index]和数组左侧的unsorted[lessIndex]交换
+            // 因为大于current的元素会移到右侧子数组unsorted[greateIndex...hi]中,因此unsorted[index]和unsorted[lessIndex]交换后依然能保证unsorted[lo,lessIndex]小于等于v
+            // 所以将lessIndex,index指向下一个元素即可,不需要再次比较unsorted[index]和current的关系
             if (compareTo < 0) {
-                exchange(unsorted, lt++, i++);
+                exchange(unsorted, lessIndex++, index++);
             }
-            // 如果大于v,则将元素unsorted[i]和数组右侧的unsorted[gt]交换
-            // 因为元素unsorted[gt]没有和v比较过,需要再次比较unsorted[i]和v的关系
+            // 如果大于v,则将元素unsorted[index]和数组右侧的unsorted[greateIndex]交换
+            // 因为元素unsorted[greateIndex]没有和current比较过,需要再次比较unsorted[index]和current的关系
             // 所以此处i不能指向下一个元素
             else if (compareTo > 0) {
-                exchange(unsorted, i, gt--);
+                exchange(unsorted, index, greateIndex--);
             }
-            // 如果unsorted[i]等于v,则不交换元素
-            else i++;
+            // 如果unsorted[index]等于v,则不交换元素
+            else index++;
         }
-        //循环结束后,存在unsorted[lo...(lt-1)] < v = unsorted[lt...gt] < unsorted[gt+1...hi]
+        //循环结束后,存在unsorted[lo...(lessIndex-1)] < current = unsorted[lessIndex...greateIndex] < unsorted[greateIndex+1...hi]
 
-        sort(unsorted, lo, lt - 1);
-        sort(unsorted, gt + 1, hi);
+        sort(unsorted, lo, lessIndex - 1);
+        sort(unsorted, greateIndex + 1, hi);
 
         return unsorted;
     }
--- src/com/github/verdgun/sort/QuickXSort.java
+++ src/com/github/verdgun/sort/QuickXSort.java
@@ -32,44 +32,44 @@ public class QuickXSort<T extends Comparable<T>> implements ISort<T> {
         exchange(unsorted, lo, m);
 
 
-        int i = lo;
-        int j = hi + 1;
+        int lessIndex = lo;
+        int greatIndex = hi + 1;
         T v = unsorted[lo];
         //找到第一个大于等于v的元素
-        while (less(unsorted[++i], v)) {
+        while (lessThen(unsorted[++lessIndex], v)) {
             //如果最后一个元素依然小于v,说明unsorted[(lo+1)...hi]都小于v,交换unsorted[lo]和unsorted[hi]之后,直接返回切分后的元素下标hi
-            if (i == hi) {
-                exchange(unsorted, i, lo);
-                return i;
+            if (lessIndex == hi) {
+                exchange(unsorted, lessIndex, lo);
+                return lessIndex;
             }
         }
 
         ////找到第一个小于等于v的元素
-        while (less(v, unsorted[--j])) {
-            //如果unsorted[lo + 1]依然大于v(unsorted[lo])则,说明unsorted[(lo+1)...hi]都大于v,直接返回切分后的元素下标lo
-            if (j == lo + 1) {
+        while (lessThen(v, unsorted[--greatIndex])) {
+            //如果unsorted[lo + 1]依然大于v(unsorted[lo]),则说明unsorted[(lo+1)...hi]都大于v,直接返回切分后的元素下标lo
+            if (greatIndex == lo + 1) {
                 return lo;
             }
         }
 
-        //如果i>=j,说明元素切分结束
-        while (i < j) {
-            //执行交换后,unsorted[i] <= v <= unsorted[j],且此时i < j
-            exchange(unsorted, i, j);
-            //因此,此内循环不会发生数组下标越界(因为必然存在unsorted[j] >= v,从而使得循环终结)
-            while (less(unsorted[++i], v)) ;
-            while (less(v, unsorted[--j])) ;
+        //如果lessIndex>=greatIndex,说明元素切分结束
+        while (lessIndex < greatIndex) {
+            //执行交换后,unsorted[lessIndex] <= v <= unsorted[greatIndex],且此时lessIndex < greatIndex
+            exchange(unsorted, lessIndex, greatIndex);
+            //因此,此内循环不会发生数组下标越界(因为必然存在unsorted[greatIndex] >= v,从而使得循环终结)
+            while (lessThen(unsorted[++lessIndex], v)) ;
+            while (lessThen(v, unsorted[--greatIndex])) ;
         }
 
-        exchange(unsorted, j, lo);
-        return j;
+        exchange(unsorted, greatIndex, lo);
+        return greatIndex;
     }
 
     private int median3(T[] unsorted, int lo, int hi) {
-        int m = lo + (hi - lo + 1) / 2;
+        int midIndex = lo + (hi - lo + 1) / 2;
 
-        int min = less(unsorted[lo], unsorted[m]) ? lo : m;
-        min = less(unsorted[min], unsorted[hi]) ? min : hi;
+        int min = lessThen(unsorted[lo], unsorted[midIndex]) ? lo : midIndex;
+        min = lessThen(unsorted[min], unsorted[hi]) ? min : hi;
         return min;
     }
 }
